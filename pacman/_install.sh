2a80514e1d781210d0f709957cc39b5a0550bcd3
Kitsunesaki Mezumona <mezumona@gmail.com>
2024-06-01 14:32:46 +0900

    add keyring

--- pacman/_install.sh
+++ pacman/_install.sh
@@ -6,10 +6,12 @@ cat \
     core.txt \
     im.txt \
     kde.txt \
+    keyring.txt \
     nvidia.txt \
     sdk.txt \
     steam-deps.txt \
     | yay -Syu --noconfirm -
 yay -Sc --noconfirm
 
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
 rustup default stable
