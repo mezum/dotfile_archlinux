fe7aa7f262c66bb86abe2e34730e3f8c3c0ef8e2
Kitsunesaki Mezumona <mezumona@gmail.com>
2024-06-01 14:11:08 +0900

    add xremap

--- user.sh
+++ user.sh
@@ -4,3 +4,4 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")"; pwd)"
 
 (cd "$BASEDIR/bash" && ./_install.sh)
 (cd "$BASEDIR/electron" && ./_install.sh)
+(cd "$BASEDIR/xremap" && ./_install.sh)
---
+++ xremap/_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eu
+
+ln -sf config.yaml $HOME/.config/xremap/config.yml
