2791a32eb5c2a3ddca1bb1ff9ccd81324c921129
Kitsunesaki Mezumona <mezumona@gmail.com>
2024-06-09 13:29:36 +0900

    Fix xremap

--- xremap/_postinstall.sh
+++ xremap/_postinstall.sh
@@ -7,7 +7,16 @@ mkdir -p \
     "$HOME/.config/xremap/" \
     "$HOME/.config/autostart/"
 
-ln -sf "$BASEDIR/config.yaml" "$HOME/.config/xremap/"
-ln -sf "$BASEDIR/xremap.desktop" "$HOME/.config/autostart/"
+cargo install xremap --features kde
+sudo gpasswd -a $USER input
 
-cargo install xremap
+sudo cp -f 65-xremap.rules /etc/udev/rules.d/
+ln -sf "$BASEDIR/config.yml" "$HOME/.config/xremap/"
+
+cat <<_EOF_ > "$HOME/.config/autostart/xremap.desktop"
+[Desktop Entry]
+Exec='$HOME/.cargo/bin/xremap' '$HOME/.config/xremap/config.yml'
+Name=xremap
+StartupNotify=true
+Type=Application
+_EOF_
--- xremap/_preinstall.sh
+++ xremap/_preinstall.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-sudo groupadd -f xremap
-sudo gpasswd -a $USER xremap
 sudo cp -f 65-xremap.rules /etc/udev/rules.d/
