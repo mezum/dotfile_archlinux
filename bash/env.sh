9876e19db43aa56e9cc3aae1e653487c282752a7
Kitsunesaki Mezumona <mezumona@gmail.com>
2024-06-27 04:59:21 +0900

    Fix electron-flags

--- bash/env.sh
+++ bash/env.sh
@@ -7,7 +7,6 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 # GBM
-export LIBVA_DRIVER_NAME="nvidia"
 export GBM_BACKEND="nvidia-drm"
 export __GLX_VENDOR_LIBRARY_NAME="nvidia"
 
@@ -20,6 +19,7 @@ export ELECTRON_OZONE_PLATFORM_HINT="auto"
 
 # VA-API
 export NVD_BACKEND="direct"
+export LIBVA_DRIVER_NAME="nvidia"
 
 # IME
 export XMODIFIERS="@im=fcitx"
